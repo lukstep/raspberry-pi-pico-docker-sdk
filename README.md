8d119ccf2eb0f39a019fc9b2fc02fe149f941647
lukstep <lukaszstepien02@gmail.com>
2022-09-17 08:52:28 +0200

    Add sample C project

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Raspberry Pi Pico Docker
+# Raspberry Pi Pico Docker SDK
 
 ## Run Docker container
 
@@ -8,4 +8,18 @@ docker build ./docker --tag pico-sdk
 docker run -d -it --name pico-sdk --mount type=bind,source=${PWD},target=/home/dev pico-sdk
 
 docker exec -it pico-sdk /bin/sh
+```
+
+## Project build
+
+After attaching to SDK container run the following command to build the project:
+```
+cd /home/dev
+
+mkdir build
+
+cd build
+
+cmake .. && make -j4
+
 ```
---
+++ main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "pico/stdlib.h"
+#include "hardware/gpio.h"
+#include "pico/binary_info.h"
+
+int LED_PIN = 25;
+
+int main ()
+{
+    bi_decl(bi_program_description("Sample binary"));
+    bi_decl(bi_1pin_with_name(LED_PIN, "on-board PIN"));
+
+    stdio_init_all();
+
+    gpio_init();
+    gpio_set_dir(LED_PIN, GPIO_OUT);
+
+    while(1)
+    {
+        gpio_put(LED_PIN, 0);
+        sleep_ms(250);
+        gpio_put(LED_PIN, 1);
+        puts("Hello Word\n");
+        sleep_ms(1000);
+    }
+}
+
