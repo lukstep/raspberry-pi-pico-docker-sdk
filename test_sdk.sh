90483caaef39a0273461ccfba5045416d1d03bb9
lukstep <lukaszstepien02@gmail.com>
2022-11-11 16:19:53 +0100

    Clone specified vesion of SDK and picotool

--- test_sdk.sh
+++ test_sdk.sh
@@ -1,4 +1,4 @@
-docker build . --tag pico-sdk
+docker build . --tag lukstep/raspberry-pi-pico-sdk:latest
 docker run -d -it --name pico-sdk --mount type=bind,source=${PWD}/test_poject,target=/home/dev pico-sdk
 docker exec pico-sdk /bin/sh -c "cd /home/dev && mkdir build && cd build && cmake .. && make -j4"
 docker exec pico-sdk /bin/sh -c "picotool"
