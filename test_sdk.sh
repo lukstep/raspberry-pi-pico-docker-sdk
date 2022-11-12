b874fd5682906ef01e406be628bc354141a27725
lukstep <lukaszstepien02@gmail.com>
2022-11-12 11:02:13 +0100

    Fix sdk_test script

--- test_sdk.sh
+++ test_sdk.sh
@@ -1,4 +1,4 @@
 docker build . --tag lukstep/raspberry-pi-pico-sdk:latest
-docker run -d -it --name pico-sdk --mount type=bind,source=${PWD}/test_poject,target=/home/dev pico-sdk
+docker run -d -it --name pico-sdk --mount type=bind,source=${PWD}/test_poject,target=/home/dev lukstep/raspberry-pi-pico-sdk:latest
 docker exec pico-sdk /bin/sh -c "cd /home/dev && mkdir build && cd build && cmake .. && make -j4"
 docker exec pico-sdk /bin/sh -c "picotool"
