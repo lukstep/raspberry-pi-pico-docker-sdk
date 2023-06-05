ac4a91fe9c3ab0f9456225f8df50b10cb22fb112
lukstep <lukaszstepien02@gmail.com>
2023-06-05 20:36:43 +0200

    Add test teardown

--- test_sdk.sh
+++ test_sdk.sh
@@ -2,3 +2,5 @@ docker build . --tag lukstep/raspberry-pi-pico-sdk:latest
 docker run -d -it --name pico-sdk --mount type=bind,source=${PWD}/test_poject,target=/home/dev lukstep/raspberry-pi-pico-sdk:latest
 docker exec pico-sdk /bin/sh -c "cd /home/dev && mkdir build && cd build && cmake .. && make -j4"
 docker exec pico-sdk /bin/sh -c "picotool"
+docker container kill pico-sdk
+docker container rm pico-sdk
